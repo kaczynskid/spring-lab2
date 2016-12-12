59dbdcf6152f00a67a0043488f467e594d94a3da
Darek Kaczyński <kaczynskid@gmail.com>
2016-12-12 11:56:57 +0100

    Dependency injection

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,20 +12,33 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
 }
 
 @RestController
 class Hello {
 
+	private Greeting greeting;
+
+	public Hello(Greeting greeting) {
+		this.greeting = greeting;
+	}
+
 	@GetMapping("/hello")
 	Greeting say() {
-		return new Greeting("Hello world changed!");
+		return greeting;
 	}
 }
 
+@Component
 class Greeting {
 
 	private String message;
+
+	Greeting() {
+		this.message = "Injected hello!";
+	}
 
 	Greeting(String message) {
 		this.message = message;
