304127b34812e5424f5ffa4443a1c6c9da9e2b5b
Darek Kaczyński <kaczynskid@gmail.com>
2016-12-12 16:14:45 +0100

    Parametrized greeting config

--- demo/src/main/java/com/greeting/GreetingConfig.java
+++ demo/src/main/java/com/greeting/GreetingConfig.java
@@ -5,22 +5,27 @@ import javax.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class GreetingConfig {
 
+	@Autowired
+	Environment environment;
+
 	@Bean
 	@Primary
 	public Greeting defaultGreeting() {
-		return new Greeting("Default greeting");
+		return new Greeting(environment.getProperty("greeting.default-msg"));
 	}
 
 	@Bean
 	public Greeting alternativeGreeting() {
-		return new Greeting("Alternative greeting");
+		return new Greeting(environment.getProperty("greeting.alternative-msg"));
 	}
 }
 
