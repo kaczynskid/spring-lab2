118e1d055ea8d2a6a993972646992fff9fc8fd9a
Darek Kaczyński <kaczynskid@gmail.com>
2016-12-12 10:17:37 +0100

    Added demo project

---
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+@RestController
+class Hello {
+
+	@GetMapping("/hello")
+	Greeting say() {
+		return new Greeting("Hello world!");
+	}
+}
+
+class Greeting {
+
+	private String message;
+
+	Greeting(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
