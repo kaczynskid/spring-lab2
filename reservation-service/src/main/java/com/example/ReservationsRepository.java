6fc7418492c0c804819dc29b808559c32705e05d
Darek Kaczyński <kaczynskid@gmail.com>
2016-12-14 14:27:25 +0100

    Added HAL browser and photo link

--- reservation-service/src/main/java/com/example/ReservationsRepository.java
+++ reservation-service/src/main/java/com/example/ReservationsRepository.java
@@ -1,16 +1,22 @@
 package com.example;
 
 import static com.example.QReservation.reservation;
+import static java.lang.String.format;
 
 import java.util.List;
 
 import com.querydsl.core.types.Predicate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.stereotype.Component;
 
 @RepositoryRestResource
 public interface ReservationsRepository extends
@@ -38,4 +44,17 @@ public interface ReservationsRepository extends
 	@Override
 	@RestResource(exported = false)
 	void deleteInBatch(Iterable<Reservation> entities);
+}
+
+@Component
+class ReservationResourceProcessor implements ResourceProcessor<Resource<Reservation>> {
+
+	@Override
+	public Resource<Reservation> process(Resource<Reservation> resource) {
+		Reservation reservation = resource.getContent();
+		String url = format("https://www.google.pl/search?tbm=isch&q=%s",
+				reservation.getName());
+		resource.add(new Link(url, "photo"));
+		return resource;
+	}
 }
